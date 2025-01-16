55a634d9b39938435061d8e56d893055a784f35a
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-01-16 10:27:30 +0900

    簡易検索

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
+
+import icon from 'astro-icon'
+import pagefind from 'astro-pagefind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: 'file',
+  },
+  integrations: [pagefind(), icon()],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
