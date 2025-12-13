08f41125dd3d32f3dc7d887f55c089318d375e9c
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-12-13 17:26:27 +0900

    feat: astro cloudflare on demand

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 // @ts-check
 
+import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import pagefind from 'astro-pagefind'
+
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.mousedev.page',
+
   build: {
     format: 'file',
   },
+
   integrations: [sitemap(), pagefind(), icon()],
+
   markdown: {
     shikiConfig: {
       theme: 'dracula',
     },
   },
+
+  adapter: cloudflare(),
 })
