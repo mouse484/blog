33a87d7568ad5e6518b3d363a48b0288109d400f
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-12-14 17:35:14 +0900

    fix(config): remove unused build and markdown configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,5 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.mousedev.page',
-  build: {
-    format: 'file',
-  },
   integrations: [sitemap(), pagefind(), icon()],
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    },
-  },
 })
